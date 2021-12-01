eb1310dfff55d9d4cfc9ba3e2fc8367c004e55a4
enikobuza <enikobuza@gmail.com>
2021-12-01 13:30:55 +0100

    css task

--- script.js
+++ script.js
@@ -26,51 +26,85 @@ Az arrownak az a hátránya, hogy metódusként nem tudjuk használni
 const inputElement = (type, name, label) => {
     return `
     <div>
-    <label>${label}</label>
+    <label>${label}</label><br>
     <input type="${type}" name="${name}">
     </div>`;
 }
 
+
+const selectElement = (type, name, label, selectOptions) => {
+    let optionElements = "";
+    for (const option of selectOptions) {
+        optionElements += `
+            <option>${option}</option>
+        `;
+    }
+    return `
+    <div>
+        <label>${label} </label>
+    
+        <${type} name="${name}">
+            ${optionElements}
+        </${type}>
+    </div>`
+}
+
+/*
+const formElement = '<form id="form">' + inputElement("text", "firstName", "Keresztneved") + inputElement("file", "profilePic", "Profilképed") + inputElement("email", "personal", "E-mail címed") + inputElement("checkbox", "newsLetter", "Hírlevelet kérek") + inputElement("checkbox", "terms", "Elfogadod a felhasználási feltételeket?") + selectElement("select", "where", "Hol hallottál rólunk?", ["internetről", "ismerőstől", "egyéb"]) + '<button>OK</button>'
+    
+
+*/
 const formElement = `
-    <form id="form">
-        ${inputElement("text", "firstName", "Keresztneved")}
-        ${inputElement("file", "profilePic", "Profilképed")}
-        ${inputElement("email", "personal", "E-mail címed")}
-        ${inputElement("radio", "newsLetter", "Hírlevelet kérek")}
-        ${inputElement("checkbox", "terms", "Elfogadod a felhasználási feltételeket?")}
-        <button>OK</button>
-    </form>
+        <form id="form">
+            ${inputElement("text", "firstName", "Keresztneved")}
+            ${inputElement("email", "personal", "E-mail címed")}
+            ${inputElement("file", "profilePic", "Profilképed")}
+            ${inputElement("checkbox", "newsLetter", "Hírlevelet kérek")}
+            ${inputElement("checkbox", "terms", "Elfogadod a felhasználási feltételeket?")}
+            ${selectElement("select", "where", "Hol hallottál rólunk?", ["internetről", "ismerőstől", "egyéb"])}
+            <button>OK</button>
+        </form>
 `;
 
 const formSubmit = (event) => {
     event.preventDefault(); /* ne töltse újra az oldalt */
     console.log(event);
-    event.target.classList.add("submitted");
+    const et = event.target;
+    et.classList.add("submitted");
+    let etValue = et.querySelector(`select[name="where"]`).value;
+    console.log(etValue);
 
 }
 
 const inputEvent = (event) => {
     console.log(event.target.value);
-    if (event.target.name === "firstName" ) {
+    if (event.target.getAttribute("name") === "firstName" ) {
         document.getElementById("inputValueContent").innerHTML = event.target.value
     };
 }
+/*
+function addImg() { 
+    let img = document.createElement('img'); 
+    img.src = "/pic01.png";
+    document.getElementById("form").appendChild(img);
+}
+*/
 
 function loadEvent() {
-    const root = document.getElementById("root");       
+    const root = document.getElementById("root");    
     root.insertAdjacentHTML("beforeend", formElement);  
     root.insertAdjacentHTML("beforeend", `              
         <div id="inputValueContent"></div>
     `);                                                 
-
+    
     const form = document.getElementById("form");       
     form.addEventListener("submit", formSubmit);        
+    //form.insertAdjacentHTML("beforeend", addImg);  
 
     const inputList = form.querySelectorAll("input");   //inputList változó(az összes input (type, name) kerül bele)
     for (const input of inputList) {
         input.addEventListener("input", inputEvent);
-    }                                                   
-
+    }   
 }
 
 window.addEventListener("load", loadEvent)
